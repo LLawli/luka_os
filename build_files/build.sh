bbd8cba129a4cc7bcf72663998e105a390571e3e
LLawli <leofilhocastro@gmail.com>
2026-03-13 21:14:57 -0300

    Add COPR repos for eza and lazygit

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,14 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux git gh neovim fzf ripgrep fd-find bat eza jq yq tree unzip wget curl rsync lsof strace shellcheck direnv gcc-c++ clang make cmake ninja-build pkgconf-pkg-config openssl-devel sqlite-devel zlib-devel libffi-devel podman podman-docker podman-compose buildah skopeo zoxide age tree-sitter delta lazygit btop
+dnf5 install -y tmux git gh neovim fzf ripgrep fd-find bat jq yq tree unzip wget curl rsync lsof strace shellcheck direnv gcc-c++ clang make cmake ninja-build pkgconf-pkg-config openssl-devel sqlite-devel zlib-devel libffi-devel podman podman-docker podman-compose buildah skopeo zoxide age tree-sitter delta btop
 dnf5 clean all
+dnf5 -y copr enable varlad/eza
+dnf5 install -y eza
+dnf5 -y copr disable varlad/eza
+dnf5 -y copr enable atim/lazygit
+dnf5 install -y lazygit
+dnf5 -y copr disable atim/lazygit
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
