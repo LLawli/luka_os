ad4867b2f6c6799de31ecb0e6a1d3030214ba83d
LLawli <leofilhocastro@gmail.com>
2026-03-13 21:24:36 -0300

    DX: add eza/lazygit via COPR and dx-groups helper script for GPU access

--- system_files/usr/etc/profile.d/10-luka-aliases.sh
+++ system_files/usr/etc/profile.d/10-luka-aliases.sh
@@ -1,4 +1,11 @@
+export EDITOR=nvim
+export VISUAL=nvim
+export BAT_THEME=TwoDark
+export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+
 alias ls='eza'
 alias ll='eza -lah'
 alias cat='bat'
 alias grep='rg'
+alias g='git'
+alias v='nvim'
