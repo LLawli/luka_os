17e6c664ae7692c66211465b14b87d5a66cbe4c8
LLawli <leofilhocastro@gmail.com>
2026-03-13 22:38:10 -0300

    Fix cleanup by avoiding removal of /run/systemd

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,33 +2,35 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+### Extra repos
 dnf5 -y copr enable alternateved/eza
-dnf5 -y copr enable atim/lazygit
-# this installs a package from fedora repos
-dnf5 install -y tmux git gh neovim fzf ripgrep fd-find bat jq yq tree unzip wget curl rsync lsof strace shellcheck direnv gcc-c++ clang make cmake ninja-build pkgconf-pkg-config openssl-devel sqlite-devel zlib-devel libffi-devel podman podman-docker podman-compose buildah skopeo zoxide age tree-sitter delta btop eza lazygit
-dnf5 -y copr disable alternateved/eza
-dnf5 -y copr disable atim/lazygit
-dnf5 clean all
+dnf5 -y copr enable dejan/lazygit
 
+### Install packages
+dnf5 install -y \
+    tmux git gh neovim \
+    fzf ripgrep fd-find bat jq yq tree unzip wget curl rsync lsof strace shellcheck direnv \
+    gcc-c++ clang make cmake ninja-build pkgconf-pkg-config openssl-devel sqlite-devel zlib-devel libffi-devel \
+    podman podman-docker podman-compose buildah skopeo \
+    zoxide age tree-sitter delta btop eza lazygit \
+    p7zip p7zip-plugins unrar \
+    mtr nmap netcat tcpdump \
+    htop iotop ncdu tldr entr progress hyperfine watchexec dive ltrace perf httpie starship
+
+### Disable extra repos after install
+dnf5 -y copr disable alternateved/eza
+dnf5 -y copr disable dejan/lazygit
+
+### Services
+systemctl enable podman.socket
+
+### Remove default alias scripts that conflict with custom aliases
 rm -f /etc/profile.d/colorls.sh /etc/profile.d/colorgrep.sh
 rm -f /etc/profile.d/colorls.csh /etc/profile.d/colorgrep.csh
 
+### Cleanup
+dnf5 clean all
 rm -rf /var/cache/*
 rm -rf /var/lib/dnf/repos/*
-rm -rf /run/dnf /run/systemd /tmp/*
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+rm -rf /run/dnf
+rm -rf /tmp/*
