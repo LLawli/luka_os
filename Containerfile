9c24f576f0506148d98f141ba08d387e2a4c17e9
LLawli <leofilhocastro@gmail.com>
2026-03-13 21:36:45 -0300

    Fix bootc lint by moving profile config to /etc and clean build leftovers

--- Containerfile
+++ Containerfile
@@ -35,7 +35,8 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=tmpfs,dst=/tmp \
     /ctx/build.sh
 
-COPY system_files/usr /usr    
+COPY system_files/etc /etc
+COPY system_files/usr /usr
 ### LINTING
 ## Verify final image and contents are correct.
 RUN bootc container lint
--- build_files/build.sh
+++ build_files/build.sh
@@ -8,16 +8,13 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux git gh neovim fzf ripgrep fd-find bat jq yq tree unzip wget curl rsync lsof strace shellcheck direnv gcc-c++ clang make cmake ninja-build pkgconf-pkg-config openssl-devel sqlite-devel zlib-devel libffi-devel podman podman-docker podman-compose buildah skopeo zoxide age tree-sitter delta btop
-dnf5 clean all
 dnf5 -y copr enable alternateved/eza
-dnf5 install -y eza
-dnf5 -y copr disable alternateved/eza
 dnf5 -y copr enable atim/lazygit
-dnf5 install -y lazygit
+# this installs a package from fedora repos
+dnf5 install -y tmux git gh neovim fzf ripgrep fd-find bat jq yq tree unzip wget curl rsync lsof strace shellcheck direnv gcc-c++ clang make cmake ninja-build pkgconf-pkg-config openssl-devel sqlite-devel zlib-devel libffi-devel podman podman-docker podman-compose buildah skopeo zoxide age tree-sitter delta btop eza lazygit
+dnf5 -y copr disable alternateved/eza
 dnf5 -y copr disable atim/lazygit
+dnf5 clean all
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
